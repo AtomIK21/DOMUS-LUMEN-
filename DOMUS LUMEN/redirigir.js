d8cfc08680132137857333d2e0008ed71e88ccc3
AtomIK21 <139806735+AtomIK21@users.noreply.github.com>
2023-07-20 01:50:35 -0600

    DOMUS-P2
    correcciones en css y agrego de funciones en js

---
+++ DOMUS LUMEN/redirigir.js
@@ -0,0 +1,14 @@
+// Esperar a que el DOM termine de cargar
+document.addEventListener("DOMContentLoaded", function () {
+    // Obtener el botón de "Iniciar Sesión" por su ID
+    const loginBtn = document.getElementById("loginBtn");
+  
+    // Agregar un evento click al botón
+    loginBtn.addEventListener("click", function () {
+      // Después de 4 segundos, redirigir a la página de carga
+      setTimeout(function () {
+        window.location.href = "pantalla_carga.html";
+      }, 4000);
+    });
+  });
+  
--- DOMUS LUMEN/saludo-in-sesion.js
+++ DOMUS LUMEN/saludo-in-sesion.js
@@ -1,45 +1,38 @@
-// Obtener referencias a los elementos del formulario y la barra de navegación
-    const aliasInput = document.getElementById('alias');
-    const emailInput = document.getElementById('email');
-    const passwordInput = document.getElementById('pass');
-    const navbarLinks = document.querySelectorAll('.navbar a');
-    const loginLink = document.querySelector('.login-link');
-
-    // Función para mostrar el saludo animado
-    function mostrarSaludo() {
-      const alias = aliasInput.value || 'Usuario';
-      const saludo = `¡Hola, ${alias}! Bienvenido/a`;
-
-      // Crear un elemento de párrafo
-      const mensaje = document.createElement('p');
-      mensaje.textContent = saludo;
-      mensaje.style.fontSize = '24px';
-      mensaje.style.textAlign = 'center';
-
-      // Animación con desvanecimiento
-      mensaje.style.opacity = 0;
-      document.body.appendChild(mensaje);
-      let opacity = 0;
-      const fadeEffect = setInterval(() => {
-        if (opacity < 1) {
-          opacity += 0.1;
-          mensaje.style.opacity = opacity;
-        } else {
-          clearInterval(fadeEffect);
-          setTimeout(() => {
-            let opacity = 1;
-            const fadeOutEffect = setInterval(() => {
-              if (opacity > 0) {
-                opacity -= 0.1;
-                mensaje.style.opacity = opacity;
-              } else {
-               clearInterval(faddOutEffect);
-                setTimeout(() => {
-                  document.body.removeChild(mensaje);
-                }, 1000);
-              }
-            }, 100);
-          }, 3000); // Ocultar después de 5 segundos (5000 ms)
-        }
-      }, 100);
-    }
+      // Obtener el formulario y el botón de inicio de sesión
+      const form = document.querySelector('.form');
+      const submitButton = document.querySelector('.submit');
+    
+      // Agregar un evento de escucha al enviar el formulario
+      form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevenir el envío del formulario
+  
+    
+        // Mostrar el saludo con el nombre del usuario
+        const saludo = document.createElement('p');
+        saludo.textContent = `Hola, Bienvenido de nuevo.`;
+        form.appendChild(saludo);
+    
+        // Aplicar animación de desvanecimiento
+        saludo.style.opacity = 0;
+        let opacity = 0;
+    
+        const fadeEffect = setInterval(() => {
+          if (opacity < 1) {
+            opacity += 0.01; // Ajusta la velocidad de desvanecimiento disminuyendo el incremento
+            saludo.style.opacity = opacity;
+          } else {
+            clearInterval(fadeEffect);
+            setTimeout(() => {
+              let fadeOutInterval = setInterval(() => {
+                if (opacity > 0) {
+                  opacity -= 0.01; // Ajusta la velocidad de desvanecimiento disminuyendo el decremento
+                  saludo.style.opacity = opacity;
+                } else {
+                  clearInterval(fadeOutInterval);
+                  saludo.remove();
+                }
+              }, 10); // Intervalo más corto para una animación más suave
+            }, 2500); // Ocultar el saludo después de 4 segundos (4000 milisegundos)
+          }
+        }, 10); // Intervalo más corto para una animación más suave
+      });
